607414d2f30874de325bfe7d09f8203ebf2550fa
Chris Barnes <barnesc@janelia.hhmi.org>
2018-07-12 16:30:34 -0400

    allow count_synapses to look for partners, not clefts

--- clefts/count_synapses.py
+++ clefts/count_synapses.py
@@ -1,12 +1,18 @@
+import os
+
 from argparse import ArgumentParser
+import glob
 
 import h5py
 import numpy as np
 
-from clefts.constants import SpecialLabel
+from clefts.constants import SpecialLabel, Dataset
 
 
-def count_clefts(path, ignore_canvas=True):
+IGNORE_CANVAS = False
+
+
+def count_clefts(path, ignore_canvas=IGNORE_CANVAS):
     with h5py.File(path, 'r') as f:
         labels_grp = f["/volumes/labels"]
         if "canvas" in labels_grp and not ignore_canvas:
@@ -19,23 +25,49 @@ def count_clefts(path, ignore_canvas=True):
     return len(unique_clefts)
 
 
+def count_partners(path):
+    with h5py.File(path, 'r') as f:
+        rows, cols = f[Dataset.PARTNERS].shape
+        assert cols == 2, f"Expected 2 columns, got {cols}"
+    return rows
+
+
+def check_cleft_per_partner(path, ignore_canvas=IGNORE_CANVAS):
+    return count_clefts(path, ignore_canvas) == count_partners(path)
+
+
+def globs_to_fpaths(*globs):
+    output = []
+    for glob_str in globs:
+        output.extend(glob.iglob(glob_str))
+    return output
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(prog="count_synapses")
     # parser.add_argument("--total", "-t", action="store_true")
+    parser.add_argument("--partners", "-p", action="store_true")
     parser.add_argument("--ignore_canvas", "-i", action="store_true")
-    parser.add_argument("path", nargs="+")
+    parser.add_argument("path", nargs="+", help="File paths or glob strings")
 
     # parsed = parser.parse_args()
+    # parsed = parser.parse_args([
+    #     "/data2/cremi/sample_*_20160501.hdf",
+    # ])
     parsed = parser.parse_args([
-        "/data2/cremi/sample_A_20160501.hdf",
-        "/data2/cremi/sample_B_20160501.hdf",
-        "/data2/cremi/sample_C_20160501.hdf",
-        # "/data2/l1_cremi/sample_alpha_padded.hdf5"
+        "--partners",
+        "/data2/manual_clefts/LN-basin/data_*.hdf5"
     ])
 
+    def fn(fpath):
+        if parsed.partners:
+            return count_partners(fpath)
+        else:
+            return count_clefts(fpath, parsed.ignore_canvas)
+
     total = 0
-    for path in parsed.path:
-        count = count_clefts(path, parsed.ignore_canvas)
+    for path in globs_to_fpaths(*parsed.path):
+        count = fn(path)
         total += count
         print(f"{path}: {count}")
 
