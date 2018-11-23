0fe592742ff206ab5c53b9659346e57e6e80aff7
Chris Barnes <barnesc@janelia.hhmi.org>
2018-11-23 09:54:17 -0500

    common files for tables

--- clefts/manual_label/tables/common.py
+++ clefts/manual_label/tables/common.py
@@ -1,5 +1,8 @@
+import os
+
+import csv
 import networkx as nx
-from typing import Iterator, Tuple, Dict, Any
+from typing import Iterator, Tuple, Dict, Any, Sequence, Optional
 
 from clefts.manual_label.skeleton import CircuitNode
 
@@ -8,3 +11,11 @@ def iter_data(g: nx.Graph) -> Iterator[Tuple[CircuitNode, CircuitNode, Dict[str,
     ndata = dict(g.nodes(data=True))
     for pre, post, edata in g.edges(data=True):
         yield ndata[pre]["obj"], ndata[post]["obj"], edata
+
+
+def write_rows(fpath: os.PathLike, data: Sequence[Sequence[Any]], headers: Optional[Sequence[str]]=None):
+    with open(fpath, 'w') as f:
+        writer = csv.writer(f)
+        if headers:
+            writer.writerow(headers)
+        writer.writerows(data)
