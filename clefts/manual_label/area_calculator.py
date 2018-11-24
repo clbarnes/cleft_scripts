d0bc4a1490c0e52e449c230d2762865cad7e3040
Chris Barnes <barnesc@janelia.hhmi.org>
2018-11-23 22:00:55 -0500

    smoothed area calculator

--- clefts/manual_label/area_calculator.py
+++ clefts/manual_label/area_calculator.py
@@ -1,4 +1,5 @@
-from scipy.ndimage import convolve
+import networkx as nx
+from scipy.ndimage import convolve, gaussian_filter1d
 from skimage.morphology import skeletonize
 from typing import Dict
 
@@ -8,6 +9,12 @@ from abc import ABCMeta, abstractmethod
 from clefts.constants import SpecialLabel, RESOLUTION
 from clefts.manual_label.constants import PX_AREA
 
+
+Y_RES = RESOLUTION["y"]
+X_RES = RESOLUTION["x"]
+DIAG_LEN = np.linalg.norm([X_RES, Y_RES])
+
+DEFAULT_SIGMA = 3*X_RES
 
 class AreaCalculator(metaclass=ABCMeta):
     def __init__(self, arr):
@@ -48,11 +55,6 @@ class SkeletonizingAreaCalculator(AreaCalculator):
         return counts
 
 
-y = RESOLUTION["y"]
-x = RESOLUTION["x"]
-diag = np.linalg.norm([x, y])
-
-
 class LinearAreaCalculator(AreaCalculator):
     """
     Skeletonize 2D planes in 3D images, convolve to find actual count of diagonal and on-face edges.
@@ -61,7 +63,7 @@ class LinearAreaCalculator(AreaCalculator):
     """
 
     # Divide by 2 because each edge will be represented twice
-    kernel = np.array([[diag, y, diag], [x, 0, x], [diag, y, diag]]) / 2
+    kernel = np.array([[DIAG_LEN, Y_RES, DIAG_LEN], [X_RES, 0, X_RES], [DIAG_LEN, Y_RES, DIAG_LEN]]) / 2
 
     origin = (0, 0)
 
@@ -83,6 +85,93 @@ class LinearAreaCalculator(AreaCalculator):
                 skeletonized = skeletonize(z_plane == label)
                 total_lengths[label] += self.length(skeletonized) * RESOLUTION["z"]
         return total_lengths
+
+
+neighbour_kernel = 2 ** np.array([
+    [4, 5, 6],
+    [3, 0, 7],
+    [2, 1, 0]
+])
+neighbour_kernel[1, 1] = 0
+
+int_reprs = np.zeros((256, 8), dtype=np.uint8)
+for i in range(255):
+    int_reprs[i] = [int(c) for c in np.binary_repr(i, 8)]
+int_reprs *= np.array([8, 7, 6, 5, 4, 3, 2, 1], dtype=np.uint8)
+
+neighbour_locs = np.array([
+    (0, 0),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1)
+])
+
+
+def im_to_graph(skeletonized: np.ndarray):
+    convolved = (
+            convolve(skeletonized.astype(np.uint8), neighbour_kernel, mode="constant", cval=0, origin=[0, 0]) * skeletonized
+    ).astype(np.uint8)
+    ys, xs = convolved.nonzero()  # n length
+
+    location_bits = int_reprs[convolved[ys, xs]]  # n by 8
+    diffs = neighbour_locs[location_bits]  # n by 8 by 2
+    g = nx.Graph()
+
+    for yx, this_diff in zip(zip(ys, xs), diffs):
+        nonself = this_diff[np.abs(this_diff).sum(axis=1) > 0]
+        partners = nonself + yx
+        for partner in partners:
+            g.add_edge(
+                yx, tuple(partner),
+                weight=np.linalg.norm(partner - yx)
+            )
+
+    return g
+
+
+def graph_to_path(g: nx.Graph):
+    start, end = [coord for coord, deg in g.degree if deg == 1]
+    return nx.shortest_path(g, start, end)
+
+
+def smooth_linestring(coords, sigma=3):
+    coords = np.asarray(coords, dtype=float)
+    return gaussian_filter1d(coords, sigma=sigma, axis=0)
+
+
+def coords_to_len(coords):
+    coords = np.asarray(coords, dtype=float)
+    norms = np.linalg.norm(np.diff(coords, axis=0), axis=1)
+    return sum(norms)
+
+
+def smooth_skel_im(skeletonized, sigma=DEFAULT_SIGMA):
+    g = im_to_graph(skeletonized)
+    path = graph_to_path(g)
+    return smooth_linestring(path, sigma)
+
+
+class GaussianSmoothedAreaCalculator(AreaCalculator):
+    def length(self, skeletonized_2d):
+        smoothed = smooth_skel_im(skeletonized_2d)
+        return coords_to_len(smoothed) * RESOLUTION['x']
+
+    def calculate(self):
+        total_areas = dict()
+        for z_plane in self.arr:
+            for label in self.label_set(z_plane):
+                if label not in total_areas:
+                    total_areas[label] = 0
+
+                skeletonized = skeletonize(z_plane == label)
+                total_areas[label] += self.length(skeletonized) * RESOLUTION['z']
+
+        return total_areas
 
 
 class TrapezoidAreaCalculator(LinearAreaCalculator):
--- clefts/tests/manual_label/area_calculator.py
+++ clefts/tests/manual_label/area_calculator.py
@@ -5,6 +5,8 @@ from clefts.manual_label.area_calculator import (
     SkeletonizingAreaCalculator,
     LinearAreaCalculator,
     TrapezoidAreaCalculator,
+    GaussianSmoothedAreaCalculator,
+    im_to_graph, graph_to_path, smooth_linestring, coords_to_len
 )
 
 
@@ -41,8 +43,18 @@ for Calculator in [
     SkeletonizingAreaCalculator,
     LinearAreaCalculator,
     TrapezoidAreaCalculator,
+    GaussianSmoothedAreaCalculator,
 ]:
     print(Calculator.__name__)
     results = Calculator(arr).calculate()
     for key, value in sorted(results.items()):
         print(f"\t{key}: {value}")
+
+print("N.B. Gaussian is small because it shrinks lines")
+
+
+path9 = graph_to_path(im_to_graph(plane9 == 9))
+len9 = coords_to_len(path9)
+len9_smooth = coords_to_len(smooth_linestring(path9, 1))
+
+print(path9, len9, len9_smooth)
